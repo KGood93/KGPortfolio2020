780b7d9e478333590c844f82eb5c77aae6d8a2d1
KGood93 <goodreau.kath@gmail.com>
2020-01-14 13:22:12 -0600

    Project Styling

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -19,7 +19,7 @@ class Projects extends Component {
                 <h2 className="sectionHead">My Projects</h2>
                 <div className="projectsPage">
                     <div className="projectScreenShots">
-                        <h3>The Covert Knitter presents: Fiber Frenzy</h3>
+                        <h3 className="projTitle">The Covert Knitter presents: Fiber Frenzy</h3>
                         <a href="https://github.com/KGood93/KGPortfolio"><img src={Portfolio_HomeSmall} alt="Start page of Fiber Frenzy Quiz" class="projImage"/></a>
                         <br/>
                         <img src={FiberFrenzyQuestion} alt="Example of question in Fiber Frenzy" className="projImage"/>
@@ -32,12 +32,12 @@ class Projects extends Component {
                             <img src={javascript} alt="fill in text" className="TechLogo" />
                             <img src={Jquery} alt="fill in text" className="TechLogo" />
                         </div>
-                        <p>Check it out : <a href="https://kgood93.github.io/FiberFrenzy/">Live</a> or <a href="https://github.com/KGood93/FiberFrenzy">Repo</a></p>
+                        <p className="projLinks">Check it out : <a href="https://kgood93.github.io/FiberFrenzy/">Live</a> or <a href="https://github.com/KGood93/FiberFrenzy">Repo</a></p>
                     </div>
                 </div>
                 <div className="projectsPage">
                     <div className="projectScreenShots">
-                        <h3>Elusive Eats</h3>
+                        <h3 className="projTitle">Elusive Eats</h3>
                         <a href="https://kgood93.github.io/ElusiveEats/"><img src={ElusiveEatsStart} alt="Start page of ElusiveEats" class="projImage"/></a>
                         <br/>
                         <img src={ElusiveEatsZip} alt="Zipcode Search Page of Elusive Eats" className="projImage"/>
@@ -52,7 +52,7 @@ class Projects extends Component {
                             <img src={javascript} alt="fill in text" className="TechLogo" />
                             <img src={Jquery} alt="fill in text" className="TechLogo" />
                         </div>
-                        <p>Check it out : <a href="https://kgood93.github.io/ElusiveEats/">Live</a> or <a href="https://github.com/KGood93/ElusiveEats">Repo</a></p>
+                        <p className="projLinks">Check it out : <a href="https://kgood93.github.io/ElusiveEats/">Live</a> or <a href="https://github.com/KGood93/ElusiveEats">Repo</a></p>
                     </div>
                 </div>
             </div>
